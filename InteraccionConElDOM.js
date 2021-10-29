525c5ad05c0a49057b59edfa63152ffd58545171
NicoGarcia <nicogarcia.ro97@gmail.com>
2021-10-29 19:40:56 +0200

    Interacción con el DOM

---
+++ InteraccionConElDOM.js
@@ -0,0 +1,29 @@
+// 1. Interacción con el DOM
+
+// ------------------------1.1-----------------------------
+
+console.log(document.querySelector(".showme"))
+
+// ------------------------1.2-----------------------------
+
+console.log(document.querySelector("#pillado"))
+
+
+// ------------------------1.3-----------------------------
+
+console.log(document.querySelectorAll("p"))
+
+
+// ------------------------1.4-----------------------------
+
+console.log(document.querySelectorAll(".pokemon"))
+
+
+// ------------------------1.5-----------------------------
+
+console.log(document.querySelectorAll("span[data-function=testMe]"))
+
+
+// ------------------------1.6-----------------------------
+
+console.log(document.querySelectorAll("span[data-function=testMe]")[2])
